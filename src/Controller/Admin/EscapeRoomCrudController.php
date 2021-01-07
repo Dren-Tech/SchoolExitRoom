53b686cf426bbda885cd8a8254625147c885a279
Marian Hahne <marian.hahne@outlook.com>
2021-01-07 22:13:46 +0100

    Config for EscapeRoom admin

--- src/Controller/Admin/EscapeRoomCrudController.php
+++ src/Controller/Admin/EscapeRoomCrudController.php
@@ -3,7 +3,12 @@
 namespace App\Controller\Admin;
 
 use App\Entity\EscapeRoom;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class EscapeRoomCrudController extends AbstractCrudController
 {
@@ -12,14 +17,27 @@ class EscapeRoomCrudController extends AbstractCrudController
         return EscapeRoom::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return parent::configureCrud($crud)
+            ->setEntityLabelInPlural("Escape Rooms")
+            ->setEntityLabelInSingular(fn(?EscapeRoom $escapeRoom) => $escapeRoom ? sprintf('"%s"', $escapeRoom->getTitle()) : 'Escape Room')
+
+            ->setPaginatorPageSize(10)
+            ->setDefaultSort(['code' => 'DESC'])
+
+            ->setHelp('new', 'Hier können Sie einen neuen Escape-Room erstellen.');
+    }
+
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('title', "Titel"),
+            TextField::new('code', "Code")->setHelp("Der Code wird zum Aufrufen des Rooms benötigt."),
+
+            FormField::addPanel("Text der Einführung")->setIcon("fa fa-file-alt"),
+            TextEditorField::new('description', "Beschreibungstext")->setHelp("Hier können einleitende Worte für den Room stehen."),
         ];
     }
-    */
 }
