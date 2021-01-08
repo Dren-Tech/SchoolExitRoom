238bbd97bf505c550c2fbc9dbb12665509c7f628
Marian Hahne <marian.hahne@outlook.com>
2021-01-08 18:24:59 +0100

    Config for Riddle admin

--- src/Controller/Admin/RiddleCrudController.php
+++ src/Controller/Admin/RiddleCrudController.php
@@ -2,8 +2,14 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Introduction;
 use App\Entity\Riddle;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class RiddleCrudController extends AbstractCrudController
 {
@@ -12,14 +18,29 @@ class RiddleCrudController extends AbstractCrudController
         return Riddle::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return parent::configureCrud($crud)
+            ->setEntityLabelInPlural("Rätsel")
+            ->setEntityLabelInSingular(fn(?Riddle $riddle) => $riddle ? sprintf('"%s"', $riddle->getTitle()) : 'Rätsel')
+
+            ->setPaginatorPageSize(25)
+            ->setDefaultSort(['identifier' => 'DESC'])
+
+            ->setHelp('new', 'Hier können Sie ein neues Rätsel für einen Escape-Room erstellen.');
+    }
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('identifier', 'Kenncode'),
+            TextField::new('title', 'Titel'),
+            AssociationField::new('escapeRoom'),
+            TextEditorField::new('text', 'Rätseltext'),
+            TextField::new('entryCode', 'Einstiegscode')->hideOnIndex(),
+            TextField::new('solutionCode', 'Lösungscode')->hideOnIndex()->setHelp("Code, der zum Lösen des Rätsels eingegeben werden muss."),
+            BooleanField::new('isUnlocked', 'Ist freigeschalten')->hideOnIndex()->setHelp("Vorerst nicht benötigt."),
+            AssociationField::new('riddleHints')->hideOnIndex(),
         ];
     }
-    */
 }
