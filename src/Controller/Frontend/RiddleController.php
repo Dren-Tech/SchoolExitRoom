beb291bb13b363b36d7a16568776ccc0865f81e7
Marian Hahne <marian.hahne@outlook.com>
2021-01-17 17:49:05 +0100

    Add check for solution code

--- src/Controller/Frontend/RiddleController.php
+++ src/Controller/Frontend/RiddleController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Frontend;
 use App\Form\RiddleCodeFormType;
 use App\Service\Entity\RiddleService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -31,14 +32,26 @@ class RiddleController extends AbstractController
     }
 
     #[Route('/riddle/{riddleIdentifier}', name: 'frontend_riddle_detail')]
-    public function detail(string $riddleIdentifier): Response
+    public function detail(string $riddleIdentifier, Request $request): Response
     {
+        $riddle = $this->riddleService->getRiddleByIdentifier($riddleIdentifier);
+
         $codeForm = $this->createForm(RiddleCodeFormType::class);
+        $codeForm->handleRequest($request);
+
+        // check if submitted code is correct
+        if ($codeForm->isSubmitted() && $codeForm->isValid()) {
+            if (strtoupper($codeForm->get("code")->getData()) == $riddle->getSolutionCode()) {
+                echo "CODE IS CORRECT";
+            } else {
+                echo "CODE IS WRONG";
+            }
+        }
 
         return $this->render('frontend/riddle/detail.html.twig', [
             'controller_name' => 'RiddleController',
             'identifier' => $riddleIdentifier,
-            'riddle' => $this->riddleService->getRiddleByIdentifier($riddleIdentifier),
+            'riddle' => $riddle,
             'code_form' => $codeForm->createView()
         ]);
     }
