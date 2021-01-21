45b6b2fe99d348b713933876a2c4b66e1dbbdb55
Marian Hahne <marian.hahne@outlook.com>
2021-01-21 21:50:14 +0100

    Add JS code to show PDF in HTML

--- assets/app.js
+++ assets/app.js
@@ -8,6 +8,9 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
+// import pdfView
+//import './pdfView';  //TODO: move to Stimulus controller
+
 import bsCustomFileInput from 'bs-custom-file-input';
 bsCustomFileInput.init();
 
